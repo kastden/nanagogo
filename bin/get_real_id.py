11921dd2d3e305a2ec014bf8fd13e5d57774fb95
K. Ast Den <post@kastden.org>
2015-07-28 17:46:17 +0200

    Add proper docstring to get_real_id.py, add example usage

--- bin/get_real_id.py
+++ bin/get_real_id.py
@@ -1,18 +1,32 @@
 #!/usr/bin/env python3
 
+"""Get a 755 user's real talkId from their public ID on the website.
+
+As an example, the ID for Furuhata Nao's page is
+Xe8jJ0D40_aWkVIvojdMdG== (http://7gogo.jp/lp/Xe8jJ0D40_aWkVIvojdMdG==), but her
+real talkId (for use with the API) is MqsG1FLTi-_9GtN76wEuUm==.
+
+Example usage and output:
+$ python3 get_real_id.py http://7gogo.jp/lp/Xe8jJ0D40_aWkVIvojdMdG== 9YFoGjThCxeWkVIvojdMdG==
+http://7gogo.jp/lp/Xe8jJ0D40_aWkVIvojdMdG== : MqsG1FLTi-_9GtN76wEuUm==
+http://7gogo.jp/lp/9YFoGjThCxeWkVIvojdMdG== : Kx09K9lOsMF9GtN76wEuUm==
+"""
+
 import re
 import yaml
 import sys
 
 import requests
 
-''' Get a 755 user's real talkId. 
-
-As an example, the ID for Furuhata Nao's page is 
-Xe8jJ0D40_aWkVIvojdMdG== (http://7gogo.jp/lp/Xe8jJ0D40_aWkVIvojdMdG==),
-but her real talkId (for use with the API) is MqsG1FLTi-_9GtN76wEuUm==.'''
 
 def get_real_id(url):
+
+    """ Get a user's talkId from their nanangogo page.
+
+    This function requests the HTML page on the given URL, extracts the
+    the settings dictionary from the embedded javascript, parses it as YAML
+    and returns their talkId.
+    """
     r = requests.get(url)
     r.raise_for_status()
 
@@ -25,6 +39,10 @@ def get_real_id(url):
 
 if __name__ == "__main__":
     args = sys.argv[1:]
+
+    if not args:
+        sys.exit(__doc__)
+
     for id in args:
         if id.startswith('http'):
             url = id
